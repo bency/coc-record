f4b181c702552e3182661b35c3a68df5b3409cef
bency@mario <bency80097@gmail.com>
2016-04-28 01:55:52 +0800

    member 加上 fillable

--- app/Member.php
+++ app/Member.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
+    protected $fillable = ['tag', 'name'];
     public function records()
     {
         return $this->hasMany('App\Member');
