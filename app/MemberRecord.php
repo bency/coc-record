8004e40bebb11c7f4663b30ada7cc29d5f80302b
bency@mario <bency80097@gmail.com>
2016-04-28 02:28:32 +0800

    加上 MemberRecord fillable

--- app/MemberRecord.php
+++ app/MemberRecord.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class MemberRecord extends Model
 {
+    protected $fillable = [
+        'tag',
+        'name',
+        'role',
+        'expLevel',
+        'league',
+        'trophies',
+        'clanRank',
+        'previousClanRank',
+        'donations',
+        'donationsReceived',
+    ];
     public function member()
     {
         return $this->belongsTo('App\Member');
