4822959f30400118bb9e3f2451d445e9cb4f9332
bency@mario <bency80097@gmail.com>
2016-04-28 01:16:57 +0800

    新增 UpdateClan Event 以新增部落基本資料

---
+++ app/Listeners/UpdateClansListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UpdateClan;
+use App\Clan;
+use App\ClanRecord;
+use App\Member;
+use App\MemberRecord;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UpdateClansListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  SomeEvent  $event
+     * @return void
+     */
+    public function handle(UpdateClan $event)
+    {
+        $data = $event->data;
+        $clan_data = array_only($data, ['name', 'tag']);
+        $clan = Clan::UpdateOrCreate($clan_data);
+    }
+}
