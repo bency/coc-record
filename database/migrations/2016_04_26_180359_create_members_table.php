daa1862fde63e38d6b27bd7d5c3d51c7c073608c
bency@mario <bency80097@gmail.com>
2016-04-28 01:15:30 +0800

    新增 member model 與 migration

---
+++ database/migrations/2016_04_26_180359_create_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('tag', 12);
+            $table->integer('clan_id');
+            $table->char('name', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('members');
+    }
+}
