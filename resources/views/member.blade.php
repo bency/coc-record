5f44110660e2f8078ea8819ff2bbefc1e5dfd2f1
bency <bency80097@gmail.com>
2016-05-10 01:12:45 +0800

    排名欄位多顯示上一次排名

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -45,6 +45,9 @@
                     <li class="mdl-list__item">
                         <span class="mdl-list__item-primary-content">
                             <i class="material-icons mdl-list__item-icon">gavel</i>
+                            From
+                            {{ $member->previousClanRank }}
+                            To
                             {{ $member->clanRank  }}
                         </span>
                     </li>
