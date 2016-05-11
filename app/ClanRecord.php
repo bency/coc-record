f12d7cbf8277ba3e10ee9fbeb239cc886988afe1
raychang <ray20204@gmail.com>
2016-05-11 10:36:28 +0800

    增加 formatClanRecord() 處理clanRecord row

--- app/ClanRecord.php
+++ app/ClanRecord.php
@@ -24,9 +24,20 @@ class ClanRecord extends Model
     {
         return $this->belongsTo('App\Clan');
     }
-    public function getBadgeUrl($badgeUrl, $type = 'large')
+    public function formatClanRecord(ClanRecord $clan_record)
     {
-        $url = json_decode($badgeUrl, true)[$type];
-        return $url;
+        $badge_url = json_decode($clan_record->badgeUrls);
+        return (Object) $ret = [
+            'id'          => $clan_record->id,
+            'clan_id'     => $clan_record->clan_id,
+            'name'        => $clan_record->name,
+            'large_icon'  => $badge_url->large,
+            'medium_icon' => $badge_url->medium,
+            'small_icon'  => $badge_url->small,
+            'level'       => $clan_record->clanLevel,
+            'wins'        => $clan_record->warWins,
+            'win_streak'  => $clan_record->warWinStreak,
+            'members'     => $clan_record->members,
+        ];
     }
 }
--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -18,6 +18,7 @@ class MemberController extends Controller
     public function showInfo($id = 0)
     {
         $clan_info = ClanRecord::findOrFail(1);
+        $clan_info = $clan_info->formatClanRecord($clan_info);
         $members = Member::orderBy('clanRank', 'asc')->get();
         if (0 == $id) {
             return view('member', ['clan' => $clan_info, 'members' => $members, 'member' => []]);
