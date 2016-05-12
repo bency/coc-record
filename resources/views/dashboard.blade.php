5168bdd3d59a2924b1c70e5989beb5f340eb5c34
bency <bency80097@gmail.com>
2016-05-12 21:22:05 +0800

    dashboard 顯示範例圖表

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,10 @@
 @extends('common.base')
 @section('content')
+<div class="row">
+    <canvas id="trophins-history"></canvas>
+</div>
 @endsection
+@push('tail-scripts')
+<script src="{{ asset('/components/Chart.js/dist/Chart.js') }}"></script>
+<script src="{{ asset('/assets/js/dashboard.js') }}"></script>
+@endpush
