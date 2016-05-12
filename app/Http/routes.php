7f7ce75d15a0b62724f516a3fce71dcb4857cb55
bency <bency80097@gmail.com>
2016-05-13 01:40:40 +0800

    建立每十分鐘一次取樣的部落總分數 api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,20 @@ Route::get('/', function () {
 
 Route::get('member', 'MemberController@showInfo');
 Route::get('member/{id}', 'MemberController@showInfo');
+Route::get('/clan/history/trophins', function () {
+    $points = \App\ClanRecord::where('created_at', ">", strtotime('-1day'))->get(['clanPoints', 'created_at'])->every(10);
+    foreach ($points as $point) {
+        $labels[] = date('Y-m-d H:i', $point->created_at->getTimestamp());
+        $datasets[] = $point->clanPoints;
+    }
+    $data = [
+        'labels' => $labels,
+        'datasets' => [
+            [
+                'label' => '部落總獎杯',
+                'data' => $datasets,
+            ],
+        ]
+    ];
+    return response()->json($data);
+});
